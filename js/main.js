d75529ffcf223a4b99f7bee2bad3d898fbae0707
shagia <mrkraiola@gmail.com>
2019-02-27 14:37:57 -0800

    add final fixes from wordpress
    All canvases redone, all text content removed besides the header, it's overall just a ScrollMagic demo now

--- js/main.js
+++ js/main.js
@@ -1,88 +1,66 @@
+/* globals ScrollMagic, TweenMax, Power4, SlowMo*/
+
 var controller = new ScrollMagic.Controller();
-TweenMax.set(".opaScroll",{autoAlpha:0});
-const gridR = document.querySelector("#split-l");
-const gridRContent = document.querySelector("#split-l > section");
-var rand = 3400;
-var i8Viewport = "(max-width: 414px)";
-
-function getDuration() {
-	//return gridR.offsetHeight - 600;
-	return gridR.offsetHeight - 600;
-}
-console.log(getDuration())
-
-var gPopIn = TweenMax.staggerFromTo('.opaScroll', 0.5,
-    {
-    	autoAlpha:0,
-    },
-    {
-      autoAlpha:1,
-    },
-    0.4
-);
-
-//var bgSwitch = TweenMax.staggerFromTo('.bg', 0.1, {backgroundColor: "#FFFFFF", color: "#000000"}, {backgroundColor: "#28190E", color: "#FFFFFF"}, 1.4);
-var headerAnimation = TweenMax.staggerFromTo('#introBody', 1, {"--faceBody-height":"-35%"}, {"--faceBody-height":"0%"}, 1.0);
-
-//var mirrorTL = TweenMax.staggerFromTo('.mi3', 1, {"opacity":"1"},{"opacity":"0"}, 0 )
-//var mirrorTL = TweenMax.staggerFrom('.mi3', 1, {"opacity":"1"}, 8); TweenMax.staggerTo('.mi3', 1, {"opacity":"0"}, 8)
-var mirrorTL = TweenMax.staggerFromTo('.mirror-img-img', 2, {autoAlpha:0, ease:Power4.easeInOut}, {autoAlpha:1, ease:Power4.easeInOut}, 1.4)
-var stopTLData = [".s-bg1", ".s-bg2"]
-var stopTL = TweenMax.staggerFromTo(stopTLData, 2, {autoAlpha:0, ease:Power4.easeInOut}, {autoAlpha:1, ease:Power4.easeInOut}, 1.4)
-
-//var headerAnimation = TweenMax.staggerFromTo('#introBody', 1, {transform: "translate(0px,0px)"}, {transform: "translate(0px , calc( var(--faceWaves-height) - var(--faceBody-height))"}, 1.0);
-
-var separationAnimation = TweenMax.staggerFromTo('.opaScrollSep', 1,
-    {
-    	autoAlpha:0,
-    },
-    {
-      autoAlpha:1,
-    },
-    1
-);
 
 
-var splitScene = new ScrollMagic.Scene({
-		triggerElement: '#split-container',
-		triggerHook: 0,
-		duration: 1200,
-		reverse: true,
-		offset: -50,
-	})
-	splitScene.setClassToggle("#split-r", "mirror-fixed");
-	splitScene.setTween(mirrorTL);
-	splitScene.addTo(controller);
+var headerAnimation = TweenMax.staggerFromTo('.lady-pos', 1, {"top":"40%", width: "60%"}, {"top":"60%", width: "30%"}, 3);
+var bottlesAnimation = TweenMax.staggerFromTo('.opaScrollSep', 1, { autoAlpha:0}, { autoAlpha:1}, 3);
+var mirrorAnimation = TweenMax.staggerFromTo('.mirror-img', 2, {autoAlpha:0, ease: SlowMo.ease.config(0.5, 0.4, false)}, {autoAlpha:1, ease: SlowMo.ease.config(0.5, 0.4, false)}, 3)
+var stopAnimation = TweenMax.staggerFromTo(['.stop-back-1','.stop-back-2'], 1, {autoAlpha:0, ease:SlowMo.ease.config(0.5, 0.4, false)}, {autoAlpha:1, ease:SlowMo.ease.config(0.5, 0.4, false)}, 0.5)
 
-var scene5 = new ScrollMagic.Scene({
-		triggerElement: '#scene5',
-		duration: 200,
-		offset: 200,
-		triggerHook: "onEnter",
-		reverse: true
-})
-	scene5.setTween(separationAnimation)
-	scene5.addTo(controller);
 
 var sceneOpen = new ScrollMagic.Scene({
-		duration: 800,
+		duration: 200,
 		offset: 0,
-		triggerHook: "onEnter",
-		reverse: true
+		reverse: true,
+    triggerElement: ".trigger-start",
 	})
-	
-	sceneOpen.setPin("#introContainer");
-	sceneOpen.setTween(headerAnimation);
-	sceneOpen.addTo(controller);
+.setTween(headerAnimation)
+.setPin(".waves-header")
+.addTo(controller);
+
+var bottlesScene = new ScrollMagic.Scene({
+		triggerElement: '.bottles-container',
+		duration: 200,
+		offset: 0,
+		// triggerHook: "onEnter",
+		reverse: true
+})
+.setTween(bottlesAnimation)
+.addTo(controller);
+
+var mirrorScene = new ScrollMagic.Scene({
+		triggerElement: '.mirror-base',
+		// triggerHook: "onEnter",
+		duration: 400,
+		reverse: true,
+		offset: 0,
+	})
+  // .addIndicators() // add indicators (requires plugin)
+	.setClassToggle("#split-r", "mirror-fixed")
+	.setTween(mirrorAnimation)
+	.addTo(controller);
 
 var sceneStopIt = new ScrollMagic.Scene({
 		triggerElement: "#sceneStop",
 		duration: 0,
-		offset: 200,
+		offset: 100,
 		triggerHook: "onEnter",
 		reverse: true,
 	})
 
-	sceneStopIt.setTween(stopTL);
+	sceneStopIt.setTween(stopAnimation);
 	sceneStopIt.addTo(controller);
 
+// var headerAnimation = TweenMax.staggerFromTo('.lady-pos', 1, {"top":"40%", width: "60%"}, {"top":"60%", width: "30%"}, 3);
+
+
+// var sceneOpen = new ScrollMagic.Scene({
+// 		duration: 150,
+// 		offset: 0,
+// 		reverse: true,
+//     triggerElement: ".trigger-start",
+// 	})
+// .setTween(headerAnimation)
+// .setPin(".waves-header")
+// .addTo(controller);
